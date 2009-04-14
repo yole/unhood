ea92238642700e270d1f2ecc59feaf1b26fd6e79
intelliyole <intelliyole@31c66c8a-273c-11de-8780-0390ca4c1f59>
2009-04-14 19:21:15 +0000

    possibility to dump all packages for a game

--- trunk/UnDump/Program.cs
+++ trunk/UnDump/Program.cs
@@ -12,21 +12,57 @@ namespace UnDump
         {
             if (args.Length != 2)
             {
-                Console.WriteLine("Usage: undump <filename> <directory>");
+                Console.WriteLine("Usage: undump <filename|directory> <directory>");
                 return;
             }
-            using (Stream s = new FileStream(args[0], FileMode.Open, FileAccess.Read, FileShare.Read))
+
+            if (Directory.Exists(args [0]))
             {
-                var package = UE3PackageReader.ReadPackage(new BinaryReader(s, Encoding.ASCII),
-                                                           Path.GetDirectoryName(args[0]));
+                DumpAllPackages(args [0], args[0], args[1]);
+            }
+            else
+            {
+                DumpPackage(args[0], Path.GetDirectoryName(args[0]), args[1]);
+            }
+        }
+
+        private static void DumpAllPackages(string gameDir, string packageDir, string outDir)
+        {
+            var entries = Directory.GetFileSystemEntries(packageDir);
+            foreach(var entry in entries)
+            {
+                var fullPath = Path.Combine(packageDir, entry);
+                if (Directory.Exists(fullPath))
+                {
+                    DumpAllPackages(gameDir, fullPath, outDir);
+                }
+                else
+                {
+                    var ext = Path.GetExtension(fullPath);
+                    if (ext.Equals(".u", StringComparison.InvariantCultureIgnoreCase) || 
+                        ext.Equals(".upk", StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        Console.WriteLine("Processing " + fullPath);
+                        DumpPackage(fullPath, gameDir, Path.Combine(outDir, Path.GetFileNameWithoutExtension(fullPath)));
+                    }
+                }
+            }
+        }
+
+        private static void DumpPackage(string packageName, string gameDir, string outDir)
+        {
+            using (Stream s = new FileStream(packageName, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                var package = UE3PackageReader.ReadPackage(new BinaryReader(s, Encoding.ASCII), gameDir);
                 foreach (var export in package.TopLevelExports)
                 {
                     if (export.ClassName == "Class")
                     {
+                        Directory.CreateDirectory(outDir);
                         var instance = (UnClass) export.ReadInstance();
                         var builder = new TextBuilder();
                         instance.Decompile(builder);
-                        var outPath = Path.Combine(args[1], export.ObjectName + ".uc");
+                        var outPath = Path.Combine(outDir, export.ObjectName + ".uc");
                         using (var fs = new FileStream(outPath, FileMode.Create))
                         {
                             using (var writer = new StreamWriter(fs, Encoding.UTF8))
@@ -37,7 +73,7 @@ namespace UnDump
                     }
                 }
 
-                ProblemRegistry.LogProblems(Path.Combine(args[1], "unhood.log"));
+                ProblemRegistry.LogProblems(Path.Combine(outDir, "unhood.log"));
             }
         }
     }
