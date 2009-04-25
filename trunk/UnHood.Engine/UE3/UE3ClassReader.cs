ede72205e9b1537d7db82791cf85080727e684d9
intelliyole <intelliyole@31c66c8a-273c-11de-8780-0390ca4c1f59>
2009-04-25 19:34:24 +0000

    recover implemented interfaces

--- trunk/UnHood.Engine/UE3/UE3ClassReader.cs
+++ trunk/UnHood.Engine/UE3/UE3ClassReader.cs
@@ -22,24 +22,38 @@ namespace UnHood.Engine.UE3
                 reader.ReadInt32();
             reader.ReadInt16();
             reader.ReadInt32();
-            int c1 = reader.ReadInt32();
-            for (int i = 0; i < c1 * 3; i++)
+            int methodCount = reader.ReadInt32();
+            for (int i = 0; i < methodCount * 3; i++)
                 reader.ReadInt32();
             reader.ReadByte();
-            for (int i = 0; i < 4; i++)
+            for (int i = 0; i < 2; i++)
                 reader.ReadInt32();
-            int c2 = reader.ReadInt32();
-            for (int i = 0; i < c2 * 2; i++)
-                reader.ReadInt32();
+            var configNameIndex = reader.ReadInt64();
+            var config = package.Names[(int) configNameIndex].Name;
+            int hideCategoriesCount = reader.ReadInt32();
+            var hideCategories = new List<string>();
+            for (int i = 0; i < hideCategoriesCount; i++)
+            {
+                var nameIndex = reader.ReadInt64();
+                var name = package.Names[(int) nameIndex].Name;
+                hideCategories.Add(name);
+            }
             int c3 = reader.ReadInt32();
             for (int i = 0; i < c3 * 3; i++)
                 reader.ReadInt32();
-            reader.ReadInt32();
+            var interfaceCount = reader.ReadInt32();
+            var interfaces = new List<UnPackageItem>();
+            for(int i=0; i<interfaceCount; i++)
+            {
+                var intfIndex = reader.ReadInt32();
+                reader.ReadInt32();
+                interfaces.Add(package.ResolveClassItem(intfIndex));
+            }
             reader.ReadInt32();
             int defaultPropertiesIndex = reader.ReadInt32();
             UnExport defaultProperties = defaultPropertiesIndex == 0
                 ? null : package.ResolveClassItem(defaultPropertiesIndex).Resolve();
-            return new UnClass(export, superIndex, bytecode, defaultProperties);
+            return new UnClass(export, superIndex, bytecode, defaultProperties, config, hideCategories, interfaces);
         }
     }
 }
